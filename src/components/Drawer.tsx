ede8ea81be824645704b34b5add68b5626472fdf
Artur Zamaliutdinov <azamalyutdinov@payneteasy.az>
2026-02-25 17:18:19 +0400

    fix(ui): glue sidebar, smooth scrolling, and improve options contrast

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -10,7 +10,7 @@ export default function Drawer({ children, isOpen, setIsOpen }: DrawerProps) {
   return (
     <main
       className={
-        " fixed overflow-hidden z-50 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out " +
+        " fixed overflow-hidden z-50 inset-0 bg-slate-950/60 backdrop-blur-[1px] transform ease-in-out " +
         (isOpen
           ? " transition-opacity opacity-100 duration-500 translate-x-0  "
           : " transition-all delay-500 opacity-0 translate-x-full  ")
@@ -18,11 +18,11 @@ export default function Drawer({ children, isOpen, setIsOpen }: DrawerProps) {
     >
       <section
         className={
-          " w-screen max-w-lg right-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  " +
+          " w-screen max-w-lg right-0 absolute h-full border-l border-slate-700 bg-slate-900 shadow-2xl delay-400 duration-500 ease-in-out transition-all transform  " +
           (isOpen ? " translate-x-0 " : " translate-x-full ")
         }
       >
-        <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
+        <article className="relative w-screen max-w-lg pb-10 flex h-full flex-col space-y-6 overflow-y-scroll text-slate-100">
           {children}
         </article>
       </section>
